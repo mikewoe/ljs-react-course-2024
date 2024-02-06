381e767e06531c045c8d0ef027c57f0b040b0741
mikewoe <jobmikewoe@gmail.com>
2024-02-06 20:05:56 +0300

    + redux: добавили redux/toolkit, перенесли логику контекстов в app;

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import {restaurants} from "./constants/mock.ts";
 import {Layout} from "./components/layout/component.tsx";
 import {RestaurantsView} from "./components/restaraunts-view/component.tsx";
+import {useCallback, useContext, useState} from "react";
+import {EMPTY_LOGIN, TUserCreds, UserContext} from "./contexts/user.tsx";
 
 export const App = () => {
+    const {user} = useContext(UserContext);
+    const [currentUser, setCurrentUser] = useState(user);
+
+    const login = useCallback((user: TUserCreds) => setCurrentUser(user), []);
+    const logout = useCallback(() => setCurrentUser(EMPTY_LOGIN), []);
+
     return (
-        <Layout>
-            <RestaurantsView restaurants={restaurants}/>
-        </Layout>
+        <UserContext.Provider value={{user: currentUser, login, logout}}>
+
+            <Layout>
+                <RestaurantsView restaurants={restaurants}/>
+            </Layout>
+        </UserContext.Provider>
     )
 }
--- src/components/layout/component.tsx
+++ src/components/layout/component.tsx
@@ -1,26 +1,18 @@
 import {Header} from "../header/component.tsx";
 import {Footer} from "../footer/component.tsx";
-import {ReactNode, useCallback, useContext, useState} from "react";
+import {ReactNode} from "react";
 import styles from "./styles.module.scss"
-import {EMPTY_LOGIN, TUserCreds, UserContext} from "../../contexts/user.tsx";
 
 export const Layout = ({children}: {children: ReactNode}) => {
-    const {user} = useContext(UserContext);
-    const [currentUser, setCurrentUser] = useState(user);
-
-    const login = useCallback((user: TUserCreds) => setCurrentUser(user), []);
-    const logout = useCallback(() => setCurrentUser(EMPTY_LOGIN), []);
-
     return (
-        <UserContext.Provider value={{user: currentUser, login, logout}}>
             <div>
                 <div className={styles.root}>
                     <Header />
                     <div>{children}</div>
                     <Footer/>
                 </div>
+
                 <div id="modal-container"/>
             </div>
-        </UserContext.Provider>
     );
 }
